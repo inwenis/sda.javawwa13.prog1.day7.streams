6dfe8d0319ab0c1f8e519a4b79985287940093d1
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-11-18 12:31:22 +0100

    implement exercise 1 - read data into objects

---
+++ src/main/java/cars/CarModel.java
@@ -0,0 +1,12 @@
+package cars;
+
+public class CarModel {
+    public int year;
+    public String division;
+    public String carline;
+    public String engineDisplay;
+    public int numberOfCylinders;
+    public int cityFuelEfficiency;
+    public int highwayFuelEfficiency;
+    public int combinedFuelEfficiency;
+}
--- src/main/java/cars/Main.java
+++ src/main/java/cars/Main.java
@@ -6,6 +6,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -14,9 +15,44 @@ public class Main {
         Path path = Paths.get(file.getAbsolutePath());
         List<String> allLInes = Files.readAllLines(path);
 
-        allLInes
+        // Model Year,Division,Carline,Eng Displ,# Cyl,City FE,Hwy FE,Comb FE
+
+        // Exercise 1 - read data from CSV into objects
+        List<CarModel> carModels = allLInes
+                .stream()
+                .skip(1) // skip headers
+                .map(x -> {
+                    String[] split = x.split(",");
+                    CarModel carModel = new CarModel();
+                    carModel.year = Integer.parseInt(split[0]);
+                    carModel.division = split[1];
+                    carModel.carline = split[2];
+                    carModel.engineDisplay = split[3];
+                    carModel.numberOfCylinders = Integer.parseInt(split[4]);
+                    carModel.cityFuelEfficiency = Integer.parseInt(split[5]);
+                    carModel.highwayFuelEfficiency = Integer.parseInt(split[6]);
+                    carModel.combinedFuelEfficiency = Integer.parseInt(split[7]);
+                    return carModel;
+                })
+                .collect(Collectors.toList());
+
+        carModels
                 .stream()
                 .limit(10)
-                .forEach(System.out::println);
+                .forEach(Main::printCarModel);
+
+    }
+
+    private static void printCarModel(CarModel x) {
+        String line = String.format("%d %s %s %s %d %d %d %d",
+                x.year,
+                x.division,
+                x.carline,
+                x.engineDisplay,
+                x.numberOfCylinders,
+                x.cityFuelEfficiency,
+                x.highwayFuelEfficiency,
+                x.combinedFuelEfficiency);
+        System.out.println(line);
     }
 }
